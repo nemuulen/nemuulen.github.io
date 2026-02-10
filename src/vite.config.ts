92c2c3f882323e2c895170e51f13363cf4d12504
Nemuulen Togtbaatar <nt152@duke.edu>
2026-02-10 13:49:06 +0800

    Change base path in Vite config to root

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // Change to '/your-repo-name/' if deploying to username.github.io/repo-name
+  base: '/', // Change to '/your-repo-name/' if deploying to username.github.io/repo-name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
